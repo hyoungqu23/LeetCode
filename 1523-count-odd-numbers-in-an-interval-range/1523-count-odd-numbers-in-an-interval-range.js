ca2d1246a41b2e227ec19434e720debe6a25b2a5
Hyoungmin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-07 21:03:59 +0900

    Time: 63 ms (92.90%), Space: 41.7 MB (86.70%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -7,12 +7,12 @@ let countOdds = function(low, high) {
     // 두 수 사이에 존재하는 숫자가 짝수라면,
     let distance = high - low + 1;
     
-    if (distance % 2 === 0) {
+    if (!(distance % 2)) {
         return distance / 2;
     }
     
     // 두 수 사이에 존재하는 숫자가 홀수라면,
-    let result = low % 2 === 1 ? (distance + 1) / 2 : (distance - 1) / 2
+    let result = low % 2 ? (distance + 1) / 2 : (distance - 1) / 2
     
     return result;
 };
